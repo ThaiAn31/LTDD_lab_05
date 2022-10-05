584c2a9a964e9f5311102f936c528b0478abdf48
ThaiAn31 <nguyenthaian31102001@gmail.com>
2022-10-05 15:02:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React,{Component} from "react";
+import { AppRegistry} from "react-native";
+
+import screen01 from "./screen/screen01";
+
+AppRegistry.registerComponent('RunProject', ()=> screen01);
---
+++ screen/screen01.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import { AppRegistry, FlatList, StyleSheet, Text, View } from "react-native";
+import flatListData from "../data/SanPham";
+
+class screen01Item extends Component{
+    render() {
+        return(
+            <View style={{flex:1, backgroundColor:''}}>
+                <Text>{this.props.item.name}</Text>
+                <Text>{this.props.item.shop}</Text>
+            </View>
+        );
+    }
+}
+
+
+export default class screen01 extends Component{
+    render() {
+        return(
+            <View style={{flex:1, marginTop:22}}>
+                <FlatList data={flatListData}
+                renderItem={({item,index})=>{
+                    return (<screen01Item item={item} index={index}>
+
+                    </screen01Item>);
+                }}>
+
+
+                </FlatList>
+
+            </View>
+
+        );
+    }
+}
